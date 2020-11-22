e0b9fe99ac5c0c5ef7f83e9e1db1a84fb8cc125a
Ze Yang <32553169+kevinyangz@users.noreply.github.com>
2020-11-22 16:25:15 -0500

    Create 114. Flatten Binary Tree to Linked List.cpp

---
+++ binaryTree/114. Flatten Binary Tree to Linked List.cpp
@@ -0,0 +1,33 @@
+    TreeNode* helper(TreeNode* root)
+    {
+        if(root==NULL) return root;
+        if(root->left==NULL && root->right==NULL ) return root;
+        
+        //rightRightMost should be the right Most node from its left subTree
+        TreeNode* rightRightMost = NULL;
+        TreeNode* leftRightMost = NULL;
+        leftRightMost = helper(root->left);
+        rightRightMost = helper(root->right);
+       
+        //we have left sub tree
+        if(root->left)
+        {
+              leftRightMost->right = root->right;
+              root->right = root->left;
+        }
+           
+        //avoid infinite loop
+        root->left= NULL;
+        
+        if(rightRightMost)
+            return rightRightMost;
+        return leftRightMost;
+        
+        
+    }
+    void flatten(TreeNode* root) {
+        //thinking this problem from the smallest sub tree 
+        //need the right most node if it has 
+        helper(root);
+        //return root;
+    }
